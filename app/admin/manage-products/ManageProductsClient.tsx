879918b76c1298df9f351b7ad7e817dcf76c20e9
Anna <sindyxin1@gmail.com>
2024-11-26 17:14:06 -0800

    Allow admin user delete existing product and update Instock status

--- app/admin/manage-products/ManageProductsClient.tsx
+++ app/admin/manage-products/ManageProductsClient.tsx
@@ -13,6 +13,12 @@ import {
 	MdRemoveRedEye
 } from 'react-icons/md';
 import ActionBtn from '@/app/components/ActionBtn';
+import { useCallback } from 'react';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import { useRouter } from 'next/navigation';
+import { ref, getStorage, deleteObject } from 'firebase/storage';
+import firebaseApp from '@/libs/firebase';
 
 interface ManageProductsClientProps {
 	products: Product[];
@@ -21,6 +27,8 @@ interface ManageProductsClientProps {
 const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
 	products
 }) => {
+	const router = useRouter();
+	const storage = getStorage(firebaseApp);
 	let rows: any = [];
 	if (products) {
 		rows = products.map((product) => {
@@ -91,11 +99,23 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
 			field: 'actions',
 			headerName: 'Actions',
 			width: 220,
-			renderCell: () => {
+			renderCell: (params) => {
 				return (
 					<div className='flex h-full gap-x-2 items-center justify-center'>
-						<ActionBtn icon={MdCached} onClick={() => {}} label='Refresh' />
-						<ActionBtn icon={MdDelete} onClick={() => {}} label='Delete' />
+						<ActionBtn
+							icon={MdCached}
+							onClick={() => {
+								handleToggleInStock(params.row.id, params.row.inStock);
+							}}
+							label='Refresh'
+						/>
+						<ActionBtn
+							icon={MdDelete}
+							onClick={() => {
+								handleDeleteProduct(params.row.id, params.row.images);
+							}}
+							label='Delete'
+						/>
 						<ActionBtn
 							icon={MdRemoveRedEye}
 							onClick={() => {}}
@@ -106,6 +126,48 @@ const ManageProductsClient: React.FC<ManageProductsClientProps> = ({
 			}
 		}
 	];
+
+	const handleToggleInStock = useCallback((id: string, inStock: boolean) => {
+		axios
+			.put('/api/products', { id, inStock: !inStock })
+			.then((res) => {
+				toast.success('Product status updated successfully');
+				router.refresh();
+			})
+			.catch((err) => {
+				toast.error('Failed to update product status');
+				console.error(err);
+			});
+	}, []);
+
+	const handleDeleteProduct = useCallback(async (id: string, images: any[]) => {
+		toast('Deleting product, please wait...');
+		const handleImageDelete = async () => {
+			try {
+				for (const item of images) {
+					if (item.image) {
+						const imageRef = ref(storage, item.image);
+						await deleteObject(imageRef);
+						console.log('Image deleted');
+					}
+				}
+			} catch (err) {
+				console.error(err);
+				toast.error('Failed to delete product images');
+			}
+		};
+		await handleImageDelete();
+		axios
+			.delete(`/api/products/${id}`)
+			.then((res) => {
+				toast.success('Product deleted successfully');
+				router.refresh();
+			})
+			.catch((err) => {
+				toast.error('Failed to delete product');
+				console.error(err);
+			});
+	}, []);
 	return (
 		<div className='max-w-[1150px] m-auto text-xl'>
 			<div className='mb-4 mt-8'>
