dbbb8e942dee7627a79c0a1a8981c212874b1015
Anna <sindyxin1@gmail.com>
2024-11-27 20:31:26 -0800

    Allow user can filter products by category

---
+++ app/components/nav/Categories.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Container from '../Container';
+import { categories } from '@/app/utils/categories';
+import Category from './Category';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+const Categories = () => {
+	const params = useSearchParams();
+	const categryFromUrl = params?.get('category');
+
+	const pathname = usePathname();
+	const isMainPage = pathname === '/';
+
+	if (!isMainPage) return null;
+
+	return (
+		<div className='bg-white'>
+			<Container>
+				<div className='pt-4 flex flex-row items-center justify-between overflow-x-auto'>
+					{categories.map((item) => (
+						<Category
+							key={item.label}
+							label={item.label}
+							icon={item.icon}
+							selected={
+								categryFromUrl === item.label ||
+								(!categryFromUrl && item.label === 'All')
+							}
+						/>
+					))}
+				</div>
+			</Container>
+		</div>
+	);
+};
+export default Categories;
