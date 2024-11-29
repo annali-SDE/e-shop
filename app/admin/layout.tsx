40731e518eebe434e48882ae546817549386fe15
Anna <sindyxin1@gmail.com>
2024-11-28 19:56:30 -0800

    Fix issue of 'metedata' is not a valid Layout export field

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,15 +1,16 @@
 import AdminNav from '../components/admin/AdminNav';
+import type { Metadata } from 'next';
 
-export const metedata = {
-  title: 'E-Shop Admin',
-  description: 'E-Shop Admin Dashboard'
-}
+export const metedata: Metadata = {
+	title: 'E-Shop Admin',
+	description: 'E-Shop Admin Dashboard'
+};
 
 const layout = ({ children }: { children: React.ReactNode }) => {
 	return (
 		<div>
 			<AdminNav />
-    {children}
+			{children}
 		</div>
 	);
 };
