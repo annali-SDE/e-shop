4f6688e4f99016adc4ea97046ae9b730f1119d03
Anna <sindyxin1@gmail.com>
2024-11-28 22:40:51 -0800

    Remove wrong config

--- next.config.js
+++ next.config.js
@@ -21,16 +21,7 @@ const nextConfig = {
 	},
 	experimental: {
 		missingSuspenseWithCSRBailout: false
-	},
-	pageExtensions: [
-		'page.tsx',
-		'page.ts',
-		// FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
-		// Instead, use `not-found.ts` as a workaround
-		// "ts" is required to resolve `not-found.ts`
-		// https://github.com/vercel/next.js/issues/65447
-		'ts'
-	]
+	}
 };
 
 module.exports = nextConfig;
