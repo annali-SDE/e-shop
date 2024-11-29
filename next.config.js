d410cd57f1ad78b37ece53507f1ed9deee7df515
Anna <sindyxin1@gmail.com>
2024-11-28 22:31:12 -0800

    try to fix deploy issues

--- next.config.js
+++ next.config.js
@@ -18,7 +18,19 @@ const nextConfig = {
 				pathname: '**'
 			}
 		]
-	}
+	},
+	experimental: {
+		missingSuspenseWithCSRBailout: false
+	},
+	pageExtensions: [
+		'page.tsx',
+		'page.ts',
+		// FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
+		// Instead, use `not-found.ts` as a workaround
+		// "ts" is required to resolve `not-found.ts`
+		// https://github.com/vercel/next.js/issues/65447
+		'ts'
+	]
 };
 
 module.exports = nextConfig;
