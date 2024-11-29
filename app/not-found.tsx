54306c73029bbe100765a624c9c08cdfaab35118
Anna <sindyxin1@gmail.com>
2024-11-28 22:35:03 -0800

    404 error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,9 @@
 // app/not-found.tsx
 
 export const dynamic = 'force-dynamic';
+import { redirect } from 'next/navigation';
 
 export default function NotFound() {
+	redirect('/');
 	return <></>;
 }
